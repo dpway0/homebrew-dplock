067179b6f81a98735b46ecff10375e3c5376ae11
dpway <dpway0@gmail.com>
2025-04-20 10:45:58 +0700

    release(v0.5.0): Brute-force Protection with Exponential Backoff

--- Formula/dplock.rb
+++ Formula/dplock.rb
@@ -1,23 +1,23 @@
 class Dplock < Formula
   desc "Simple, secure CLI password manager written in Rust"
   homepage "https://github.com/dpway0/dplock"
-  version "0.4.1"
+  version "0.5.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/dpway0/dplock/releases/download/v0.4.1/dplock-0.4.1-macos-arm64.tar.gz"
-      sha256 "9be9663163c730734e88629dad25a75233945656b14ffb7cce1250b817d229bc"
+      url "https://github.com/dpway0/dplock/releases/download/v0.5.0/dplock-0.5.0-macos-arm64.tar.gz"
+      sha256 "7b64fa356ca61c2cee0e0905fd3bece841d1587524fc09b6a5b914336278277e"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/dpway0/dplock/releases/download/v0.4.1/dplock-0.4.1-macos-x86_64.tar.gz"
-      sha256 "ea161acb55e2219ba245d3ad3aaa8bbae4c88a64ecd921623387b8991f90ed98"
+      url "https://github.com/dpway0/dplock/releases/download/v0.5.0/dplock-0.5.0-macos-x86_64.tar.gz"
+      sha256 "f5869ff5539745df6587aa9556e185f3342270ddb4ce26d2943750111bb7f805"
     end
   end
 
   on_linux do
-    url "https://github.com/dpway0/dplock/releases/download/v0.4.1/dplock-0.4.1-linux-x86_64.tar.gz"
-    sha256 "8c028d33873b947bbc6784edd3bb72d809abe1762509bfefc51089a14d5866dd"
+    url "https://github.com/dpway0/dplock/releases/download/v0.5.0/dplock-0.5.0-linux-x86_64.tar.gz"
+    sha256 "405528bceb5bc8cd60e98e795597028589d605c4811a6140beb6301dfed67a68"
   end
 
   def install
